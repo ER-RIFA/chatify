56048aeb8f95b3d9568ead82d13c185711ae76e7
ER-RIFA <rrifa0609@gmail.com>
2026-08-04 16:04:09 +0530

    Welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import User from "../models/User.js";
 import bcrypt from"bcryptjs";
 import { generateToken } from "../lib/utils.js";
+import { ENV } from "../lib/env.js";
 
 export const signup = async(req, res) => {
     const {fullName, email, password} = req.body
@@ -40,11 +42,17 @@ export const signup = async(req, res) => {
             generateToken(savedUser._id, res);
 
             res.status(201).json({
-                _id:newUser,
+                _id:newUser._id,
                 fullName:newUser.fullName,
                 email:newUser.email,
                 profilePic:newUser.profilePic
             });
+
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+            } catch (error) {
+                console.error("Failed to send the welcome email: ", error);
+            }
         } else {
             res.status(400).json({message : "Invalid user data"});
         }
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,11 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
     try {
-        const { MONGO_URI } = process.env;
-        if (MONGO_URI) throw new Error ("MONGO_URI is not set");
-        console.log(process.env.MONGO_URI);
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const { MONGO_URI } = ENV;
+        if (!MONGO_URI) throw new Error ("MONGO_URI is not set");
+        const conn = await mongoose.connect(ENV.MONGO_URI);
         console.log("MONGOBD CONNECTED : ", conn.connection.host);
     } catch (error) {
         console.log("Error connection to MONGODB : ", error);
