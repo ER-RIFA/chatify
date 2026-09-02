e735f8b753051aaaaccd23aa25606a4d5e46e600
ER-RIFA <rrifa0609@gmail.com>
2026-09-03 00:32:37 +0530

    Fix axios instance export

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,4 +1,6 @@
-const axiosInstance = axios.create({
+import axios from "axios";
+
+export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
